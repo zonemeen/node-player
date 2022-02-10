3f615b4c53b5a21b9ace743d67aecb66b870c43f
miqilin21 <miqilin18@gmail.com>
2022-02-10 17:07:13 +0800

    feat: playback of online music resources

--- example/example.js
+++ example/example.js
@@ -6,7 +6,9 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 const filePath = path.join(__dirname, 'foo.mp3')
 const player = new Player()
-player.play(filePath)
+player.play(
+  'https://cdn.jsdelivr.net/gh/miqilin21/static@master/img/Prospect.mp3'
+)
 setTimeout(() => {
   player.kill()
 }, 5000)
--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,78 @@
+import fs from 'fs'
+import https from 'https'
+import http from 'http'
+import path from 'path'
 import { command } from 'execa'
 
 /* MAC PLAY COMMAND */
-const macPlayCommand = (path, volume) => `afplay \"${path}\" -v ${volume}`
+const macPlayCommand = (src, volume) => `afplay \"${src}\" -v ${volume}`
 
 /* WINDOW PLAY COMMANDS */
 const addPresentationCore = `Add-Type -AssemblyName presentationCore;`
 const createMediaPlayer = `$player = New-Object system.windows.media.mediaplayer;`
-const loadAudioFile = (path) => `$player.open('${path}');`
+const loadAudioFile = (src) => `$player.open('${src}');`
 const setAudioVolume = (volume) => `$player.Volume = ${volume};`
 const playAudio = `$player.Play();`
 const stopAudio = `Start-Sleep 1; Start-Sleep -s $player.NaturalDuration.TimeSpan.TotalSeconds;Exit;`
 
-const windowPlayCommand = (path, volume) =>
+const windowPlayCommand = (src, volume) =>
   `powershell -c ${addPresentationCore} ${createMediaPlayer} ${loadAudioFile(
-    path
+    src
   )} ${setAudioVolume(volume)} ${playAudio} ${stopAudio}`
 
-let subprocess
+let subProcess, filePath, isLocal
 
 class Player {
-  play(path: string, volume: number = 0.5) {
+  play(src: string, volume: number = 0.5) {
     /**
      * Window: mediaplayer's volume is from 0 to 1, default is 0.5
      * Mac: afplay's volume is from 0 to 255, default is 1. However, volume > 2 usually result in distortion.
      * Therefore, it is better to limit the volume on Mac, and set a common scale of 0 to 1 for simplicity
      */
-    const volumeAdjustedByOS =
-      process.platform === 'darwin' ? Math.min(2, volume * 2) : volume
-
-    const playCommand =
-      process.platform === 'darwin'
-        ? macPlayCommand(path, volumeAdjustedByOS)
-        : windowPlayCommand(path, volumeAdjustedByOS)
     try {
-      subprocess = command(playCommand)
+      const volumeAdjustedByOS =
+        process.platform === 'darwin' ? Math.min(2, volume * 2) : volume
+
+      isLocal = !(src.indexOf('http') === 0 || src.indexOf('https') === 0)
+
+      let playCommand
+
+      if (isLocal) {
+        playCommand =
+          process.platform === 'darwin'
+            ? macPlayCommand(src, volumeAdjustedByOS)
+            : windowPlayCommand(src, volumeAdjustedByOS)
+
+        subProcess = command(playCommand)
+      } else {
+        const $http = src.indexOf('http') === 0 ? https : http
+        $http.get(src, (res) => {
+          const fileSteam = fs.createWriteStream('temp.mp3')
+          res.pipe(fileSteam)
+
+          filePath = path.join(process.cwd(), 'temp.mp3')
+
+          playCommand =
+            process.platform === 'darwin'
+              ? macPlayCommand(filePath, volumeAdjustedByOS)
+              : windowPlayCommand(filePath, volumeAdjustedByOS)
+
+          subProcess = command(playCommand)
+
+          fileSteam.on('finish', () => {
+            fileSteam.end()
+          })
+        })
+      }
     } catch (err) {
       throw err
     }
   }
   kill() {
-    subprocess.kill()
+    subProcess.kill()
+    if (!isLocal) {
+      fs.unlinkSync(filePath)
+    }
   }
 }
 
